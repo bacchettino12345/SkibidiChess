b747b2ab5c55d201f7f555632b5ec62a47425ca2
Riccardo Cinaglia <cinagliariccardo@gmail.com>
2025-02-13 23:14:56 +0100

    fix API Stockfish

--- Backend/game/js/Stockfish.js
+++ Backend/game/js/Stockfish.js
@@ -1,15 +1,14 @@
-import {Helper} from "../../../src/js/game/Helpers.js"
-  
-
-export function callAPI(fen, depth) {
+export async function callAPI(fen, depth) {
   const url = new URL('https://stockfish.online/api/s/v2.php');
   url.searchParams.append('fen', fen);
   url.searchParams.append('depth', depth);
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => data.bestmove.substring(9,13))
-    .catch(error => {
-      console.error('Errore:', error);
-      throw error;
-    });
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.bestmove.substring(9, 13);
+  } catch (error) {
+    console.error('Errore durante la chiamata API:', error);
+    throw error;
+  }
 }
